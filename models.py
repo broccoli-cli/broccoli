be346690c067ec105f9adce70c62285a5ad1799d
elifarslancelik <elifnurarslnclk@gmail.com>
2025-07-11 01:53:01 +0300

    Update models.py
    -add new model for messages

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -9,4 +10,15 @@ class User(db.Model):
     password_hash = db.Column(db.String(256), nullable=False)
 
     def __repr__(self):
-        return f'<User {self.username}>' 
+        return f'<User {self.username}>'
+
+class Message(db.Model):
+    """Chat mesajlarını tutan model."""
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('messages', lazy=True))
+
+    def __repr__(self):
+        return f'<Message {self.id} by {self.user.username}>' 
