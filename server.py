f26e4b5975e6c7ad986dd299573b49f83b0f5e5f
elifarslancelik <elifnurarslnclk@gmail.com>
2025-07-11 03:01:35 +0300

    Update server.py

--- server.py
+++ server.py
@@ -45,4 +45,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         
-    socketio.run(app, host='0.0.0.0', port=8080, debug=True) 
+    socketio.run(app, host='127.0.0.1', port=8080, debug=True) 
